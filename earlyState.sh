f05952c3a735748a725634d52162e120c68f676c
Matthias Toberer <matthias.toberer@uni-ulm.de>
2019-02-08 10:22:20 +0100

    cleanup

--- earlyState.sh
+++ earlyState.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-printf "%s" "reset duet ..."
-curl `https://$IP/rr_gcode?gcode=M999`
 
-printf "%s" "waiting for response ..."
-while ! timeout 0.2 ping -c 1 -n $1 &> /dev/null
+if [ $# -eq 0 ]   # check if we have a target
+  then
+    echo "No target set! Use $0 {IP}"
+    exit 0
+fi
+
+clear
+printf "%s" "send M999 (reset) to duet ..."
+URL_RESPONSE=`curl 'http://$1/rr_gcode?gcode=M999'`    # https://reprap.org/wiki/G-code#M999:_Restart_after_being_stopped_by_error
+echo "$CURL_RESPONSE \n\n"
+
+printf "%s" "waiting for response ...\n"
+while ! ping -c1 $1 &>/dev/null
 do
-    for i in 1 2 3;
-      do
-        CURL_RESPONSE=`curl 'http://$IP/rr_status?type=$i'`
-    done
+  #echo "waiting for duet ob $1 ... \n"
+  printf "%c" "."
 done
-printf "\n%s\n"  "Duet is back..."
+
+for i in 1 2 3; 
+  do
+    CURL_RESPONSE=`curl "http://${1}/rr_status?type=$i"`
+    echo "rr_status=$i\n"
+    echo "$CURL_RESPONSE \n\n"
+done
+
+
+
+printf "\n%s\n"  "duet is back..."
